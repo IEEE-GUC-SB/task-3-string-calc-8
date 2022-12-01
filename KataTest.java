d47d41331fac4a05d0dc464d8c91a82f2ec42ead
M-Bahy <117231085+M-Bahy@users.noreply.github.com>
2022-12-02 01:40:39 +0200

    ❌ failing unknownNumberOfDigitsTest

--- KataTest.java
+++ KataTest.java
@@ -25,5 +25,10 @@ class KataTest {
 		String two = "1,2";
 		assertEquals(3,kata.Add(two));
 	}
+	@Test
+	void testUnknownNumberOfDigits() {
+		String numbers = "1,2,3,4,5,6,7,8,9";
+		assertEquals(45,kata.Add(numbers));
+	}
 
 }
