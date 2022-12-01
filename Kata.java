ec6b1b5004acce1f483ca79453ba4a0035ec0051
M-Bahy <117231085+M-Bahy@users.noreply.github.com>
2022-12-02 01:19:52 +0200

    🔄 refactored sumEmptyString Again

--- Kata.java
+++ Kata.java
@@ -3,10 +3,14 @@ import java.util.StringTokenizer;
 public class Kata {
 	public  int Add(String numbers) {
 		StringTokenizer stringTokenizer = new StringTokenizer(numbers,",");
-		if(stringTokenizer.countTokens() == 0) {
+		if(isEmpty(stringTokenizer)) {
 			return 0;
 		}
 		return -1;
+	}
+
+	private boolean isEmpty(StringTokenizer stringTokenizer) {
+		return stringTokenizer.countTokens() == 0;
 	}
 
 }
