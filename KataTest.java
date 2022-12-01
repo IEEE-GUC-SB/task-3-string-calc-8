58534062fd75250ac636695fe4a2673b182ac963
M-Bahy <117231085+M-Bahy@users.noreply.github.com>
2022-12-02 00:52:34 +0200

    Created the files

---
+++ KataTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class KataTest {
+
+	@BeforeEach
+	public void setUp() {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
